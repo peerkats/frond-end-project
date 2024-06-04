52d80de4e0f6813b2813c993b4b5f795123154bf
peer <peerkats@gmail.com>
2024-06-05 00:26:54 +0200

    added a custom img for the site changed the sections and did some small other stuff

--- python/telegrambot/main.py
+++ python/telegrambot/main.py
@@ -9,23 +9,7 @@ def send_welcome(message):
 
 
 
-import http.client
-import urllib.parse
 
-params = urllib.parse.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'})
-headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-
-conn = http.client.HTTPSConnection("bugs.python.org")
-
-conn.request("POST", "", params, headers)
-
-response = conn.getresponse()
-print(response.status, response.reason)
-
-data = response.read()
-print(data)
-
-conn.close()
 
 
 
